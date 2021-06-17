4e2330ed0e73e6bb12eda043f78393e69a367e5e
Rob Newsom <rwnewsom@gmail.com>
2021-06-17 11:27:31 -0400

    ERD Validated offline - initial model class constructed.

---
+++ Capstone/Capstone/Models/City.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Capstone.Models
+{
+    public class City
+    {
+        /// <summary>
+        /// The city id.
+        /// </summary>
+        public int CityID { get; set; }
+
+        /// <summary>
+        /// The name of the city.
+        /// </summary>
+        public string CityName { get; set; }
+
+        /// <summary>
+        /// Official two letter abbreviation of state.
+        /// </summary>
+        public string StateAbbreviation { get; set; }
+    }
+}
