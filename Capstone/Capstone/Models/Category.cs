aebe6c804ca866d0518a000a02d5fdd200bcf850
Amman Bahta <amman.bahta@gmail.com>
2021-06-17 11:39:07 -0400

    capstone progress

---
+++ Capstone/Capstone/Models/Category.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Capstone.Models
+{
+    public class Category
+    {
+        /// <summary>
+        /// The category id
+        /// </summary>
+        public int CategoryId { get; set; } 
+
+
+
+        /// <summary>
+        /// The category name
+        /// </summary>
+        public string CategoryName { get; set; }
+
+
+    }
+}
---
+++ Capstone/Capstone/Models/State.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Capstone.Models
+{
+   public class State
+    {
+        /// <summary>
+        /// The state abbreviation
+        /// </summary>
+        public string StateAbbreviation { get; set; }
+
+        /// <summary>
+        /// The state name
+        /// </summary>
+        public string StateName { get; set; }
+    }
+}
